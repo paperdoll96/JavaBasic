759a3aaa8e09f2fa320d49be09f49abdad0c3b89
82105 <ijh7476@naver.com>
2024-04-02 19:47:57 +0900

    - 인터페이스 implements

---
+++ src/poly/ex5/Dog.java
@@ -0,0 +1,15 @@
+package poly.ex5;
+
+// 인터페이스를 상속 받을때는 extends 대신에 implements(구현) 키워드 사용
+// 그래서 인터페이스는 상속이라 하지 않고 구현이라 한다
+public class Dog implements InterfaceAnimal{
+    @Override
+    public void sound() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
